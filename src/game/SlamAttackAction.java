37650fa0b4a90564917a20938dfc6d2aecb661d5
zche0160 <chen.zilei@hotmail.com>
2023-04-22 13:26:52 +1000

    Updating SlamAttackAction constructor

--- src/game/SlamAttackAction.java
+++ src/game/SlamAttackAction.java
@@ -3,11 +3,17 @@ package game;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.Weapon;
+
+import java.util.ArrayList;
 
 public class SlamAttackAction extends AttackAction implements Behaviour{
 
-    public SlamAttackAction() {
-        super()
+    private ArrayList<AttackAction> attackList;
+
+    public SlamAttackAction(Actor target, String direction, Weapon weapon) {
+        super(target, direction, weapon);
+        this.attackList = new ArrayList<AttackAction>();
     }
 
     @Override
