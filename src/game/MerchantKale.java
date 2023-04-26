9943024a8b83b4de286582661a20ff84ae5adad5
kman0030 <kman0030@student.monash.edu>
2023-04-26 09:56:29 +0000

    Began implementation of Sell action with MerchantKale. Trying to use...

--- src/game/MerchantKale.java
+++ src/game/MerchantKale.java
@@ -2,10 +2,12 @@ package game;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
-import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+import java.util.ArrayList;
 
 public class MerchantKale extends Actor {
     /**
@@ -21,11 +23,33 @@ public class MerchantKale extends Actor {
     private final int UCHIGATANA_SELL_PRICE = 500;
     private final int GREAT_KNIFE_SELL_PRICE = 350;
     private final int CLUB_SELL_PRICE = 100;
-    private final String[] PURCHASABLE_ITEMS = {"Uchigatana", "Great Knife", "Club"};
-    private final String[] SELLABLE_ITEMS = {"Uchigatana", "Great Knife", "Club", "Grossmesser"};
+    private Uchigatana uchigatana = new Uchigatana();
+    private GreatKnife greatKnife = new GreatKnife();
+    private Club club = new Club();
+    private Grossmesser grossmesser = new Grossmesser();
+    private final String[] PURCHASABLE_ITEMS = {"Uchigatana", "GreatKnife", "Club"};
+    private final ArrayList<WeaponItem> SELLABLE_ITEMS = new ArrayList<>();
     public MerchantKale(String name, char displayChar, int hitPoints) {
         super("Merchant Kale", 'K', 500);
+        SELLABLE_ITEMS.add(uchigatana);
+        SELLABLE_ITEMS.add(greatKnife);
+        SELLABLE_ITEMS.add(club);
+        SELLABLE_ITEMS.add(grossmesser);
     }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        for(WeaponItem i : SELLABLE_ITEMS) {
+            //if (otherActor.getWeaponInventory().get(i). )
+            if (otherActor.getWeaponInventory().contains(i)) {
+                // uchigatana there for the sake of no errors, ideally is just i
+                actions.add(new SellAction(i, uchigatana));
+            }
+        }
+        return actions;
+    }
+
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
         return null;
---
+++ src/game/SellAction.java
@@ -0,0 +1,42 @@
+package game;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+public class SellAction extends Action {
+    // private Uchigatana uchigatana = new Uchigatana();
+    private WeaponItem weaponItem;
+    private Uchigatana uchigatana;
+
+    public SellAction(WeaponItem weaponItem, Uchigatana uchigatana) {
+        this.weaponItem = weaponItem;
+        this.uchigatana = uchigatana;
+    }
+
+    /**
+     * Perform the Action.
+     *
+     * @param actor The actor performing the action.
+     * @param map   The map the actor is on.
+     * @return a description of what happened that can be displayed to the user.
+     */
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        // updateNumberOfRunes(uchigatana.getSellPrice());
+        //actor.removeItemFromInventory(uchigatana);
+        return "player + sold uchigatana + for 500";
+    }
+
+    /**
+     * Returns a descriptive string
+     *
+     * @param actor The actor performing the action.
+     * @return the text we put on the menu
+     */
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
+}
